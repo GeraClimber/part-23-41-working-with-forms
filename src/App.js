56378db1af8036ce554e2f7d631e39f5292b0578
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-05-11 14:19:51 +0300

    add textarea checkbox etc

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ class App extends Component {
         super()
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: true,
+            gender: "",
+            favColor: "blue"
         }
         this.handleChange = this.handleChange.bind(this)
     }
     
     handleChange(event) {
-      this.setState({
-          [event.target.name]: event.target.value
-      })
-  }
+      const {name, value, type, checked} = event.target
+      type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+    }
     
     render() {
         return (
@@ -34,7 +36,72 @@ class App extends Component {
                     placeholder="Last Name" 
                     onChange={this.handleChange} 
                 />
+                
+                {
+                    /**
+                     * Другие полезные элементы форм:
+                     * 
+                     * <textarea /> 
+                     * <input type="checkbox" />
+                     * <input type="radio" />
+                     * <select> и <option> 
+                     */
+                }
+
+                <br />
+                <textarea 
+                  name="textArea"
+                  value={this.state.textArea}
+                  onChange={this.handleChange}
+                />
+                <br />
+                <label>
+                  <input 
+                      type="checkbox" 
+                      name="isFriendly"
+                      checked={this.state.isFriendly}
+                      onChange={this.handleChange}
+                  /> Is friendly?
+                </label>
+                <br />
+                <br />
+                <label>
+                  <input 
+                    type="radio" 
+                    name="gender"
+                    value="male"
+                    checked={this.state.gender === "male"}
+                    onChange={this.handleChange}
+                  /> Male
+                </label>
+                <br />
+                <label>
+                  <input 
+                    type="radio" 
+                    name="gender"
+                    value="female"
+                    checked={this.state.gender === "female"}
+                    onChange={this.handleChange}
+                  /> Female
+                </label>
+
+                <br />
+                <label>Favorite Color:</label>
+                  <select 
+                    value={this.state.favColor}
+                    onChange={this.handleChange}
+                    name="favColor"
+                  >
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                  </select>
+
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
+                <h2><font color="#3AC1EF">You are a {this.state.gender}</font></h2>
+                <h2><font color="#3AC1EF">Your favorite color is {this.state.favColor}</font></h2>
             </form>
         )
     }
